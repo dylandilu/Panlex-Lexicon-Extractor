43fbefcbb189835c9378b823a68e654bf56c13e7
Di <lud2@rpi.edu>
2018-05-02 02:56:05 -0400

    shell example update

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-python panlex_bilingual_extract.py --source_language=ben --target_language=spa --output_directory=data/lexicons
+python panlex_bilingual_extract.py --source_language=spa --target_language=spa --output_directory=data/lexicons
 
